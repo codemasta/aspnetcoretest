0e24209ef8b6b232325b2e2d9cfb1f0dbc154bb5
Erick Window-PC <erickchidubem@gmail.com>
2020-10-12 23:32:39 +0100

    add test

---
+++ InternetBankingRESTfulService.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InternetBankingRESTfulService.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
